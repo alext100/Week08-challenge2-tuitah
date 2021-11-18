5850583a414ee585033c5a5be50b0275a9958f43
SandraQS <sanquesan@gmail.com>
2021-11-18 17:48:46 +0100

    Modify classes for to use module.css

--- components/TuitCard/TuitCard.js
+++ components/TuitCard/TuitCard.js
@@ -1,19 +1,23 @@
-import "./TuitCard.module.css";
 import TimeAgo from "javascript-time-ago";
 import es from "javascript-time-ago/locale/es.json";
 import ReactTimeAgo from "react-time-ago";
+import styles from "../../styles/TuitCard.module.css";
 
 TimeAgo.addLocale(es);
 
 const TuitCard = ({ tuit }) => (
   <>
-    <article className="tuit-card col-md-5 col-12 row justify-content-center">
+    <article
+      className={`${styles["tuit-card"]} col-md-5 col-12 row justify-content-center`}
+    >
       <div className="col-12 row">
-        <p className="tuit-card__text col-12">{tuit.text}</p>
-        <p className="tuit-card__date col-md-9 col-12">
+        <p className={`${styles["tuit-card__text"]} col-12`}>{tuit.text}</p>
+        <p className={`${styles["tuit-card__date"]}  col-md-9 col-12`}>
           <ReactTimeAgo date={Date.parse(tuit.date)} locale="es" />
         </p>
-        <p className="tuit-card__likes col-md-3 col-12">Likes: {tuit.likes}</p>
+        <p className={`${styles["tuit-card__likes"]} col-md-3 col-12`}>
+          Likes: {tuit.likes}
+        </p>
       </div>
       <div className="col-12 row">
         <button type="button" className="btn btn-dark col-md-6 col-12">
